877b48d5529a20d1f4eb7b5ac2a13fe5dd80e39c
Christian Ramirez <christian.r0602@gmail.com>
2022-07-05 12:12:09 -0500

    celsius to fahrenheit

--- Fundamentals_Todo_2/script.js
+++ Fundamentals_Todo_2/script.js
@@ -106,3 +106,14 @@
 //   return 5 * (fDegrees - 32) / 9
 // }
 // console.log(fahrenheitToCelsius(54))
+
+// Celsius to Fahrenheit
+// Create celsiusToFahrenheit(cDegrees) that accepts number of degrees Celsius, and returns the equivalent temperature expressed in Fahrenheit degrees.
+
+// (Optional): Do Fahrenheit and Celsius values equate at a certain number? The scientific calculation can be complex, so for this challenge just try a series of Celsius integer values starting at 200, going downward (descending), checking whether it is equal to the corresponding Fahrenheit value.
+
+// function celsiusToFahrenheit(cDegrees){
+//   return (9/5 * cDegrees) + 32
+// }
+
+// console.log(celsiusToFahrenheit(200))
